085f20d51491488e07320ea41d11db1e5e0ed342
pakito072 <105859870+pakito072@users.noreply.github.com>
2024-11-20 11:46:24 +0100

    js full changed
    Tenia (practicamente) todo el js mal hecho por querer copiarlo del chat gpt, ahora aunque sea crea y edita (no del todo bien)

--- CrudJsTareasPendientes/src/resources/js/script.js
+++ CrudJsTareasPendientes/src/resources/js/script.js
@@ -15,125 +15,121 @@ class Tarea {
 
 class TareaManager {
   constructor() {
-    this.task = this.loadTask()
+    this.tasks = this.loadTasks()
+
+  }
+
+  loadTasks() {
+    const cookies = Cookies.get("tasks")
+    if(cookies) {
+      const tasksArray = JSON.parse(cookies)
+      return tasksArray.map(task => new Tarea(task.id, task.description))
+    }
+
+  }
+
+  saveTasks() {
+    Cookies.set("tasks", JSON.stringify(this.tasks), { expires: 7 })
 
   }
 
   addTask(description) {
-    const id = this.task.length ? this.task[this.task.length - 1].id + 1 : 1
+    const id = this.tasks.length ? this.tasks[this.tasks.length - 1].id + 1 : 1
 
-    const newTask = new Task(id, description)
-    this.task.push(newTask);
-    this.saveTask()
-
-    return newTask
+    const newTask = new Tarea(id, description)
+    this.tasks.push(newTask);
+    this.saveTasks()
+    this.readTasks()
   }
-
-  readTask() {
-    return this.task
-
-  }
-
 
   updateTask(id, description) {
-    const task = this.task.find(function (task) {
+    const task = this.tasks.find(function (task) {
       return task.id === id
     })
 
     if (task) {
       task.setDescription(description)
-      this.saveTask()
-      return task
+      this.saveTasks()
+      this.readTasks()
 
     }
-    return null
   }
 
   deleteTask(id, description) {
-    const index = this.task.find(function (task) {
-      return task.id === id
+    this.tasks = this.tasks.filter(function (task) {
+      return task.id !== id
     })
 
-    if (index !== -1) {
-      const taskTerminated = this.task.splice(index, 1)[0]
-      this.saveTask()
-
-      return taskTerminated
-
-    }
-    return null
-  }
-
-  saveTask() {
-    document.cookie = "Tareas=" + JSON.stringify(this.task) + "; path=/; max-age=3600"
+    this.saveTasks()
+    this.readTasks()
 
   }
 
-  loadTask() {
-    const cookies = document.cookie.split("; ").find(function (row) {
-      return row.startsWith("Tareas=")
+  readTasks() {
+    const taskBody = document.getElementById("taskBody")
+    taskBody.innerHTML = ""
+    this.tasks.forEach(function (task) {
+      const row = document.createElement("tr")
+      row.innerHTML = 
+      `
+        <td>${task.id}</td>
+        <td>${task.description}</td>
+        <td class="d-grid gap-2">
+          <button class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#addTaskModal" onclick="prepareEditTask(${task.id})">Modificar</button>
+          <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#delTaskModal" onclick="prepareDeleteTask(${task.id})">Eliminar</button>
+        </td>
+      `
+      taskBody.appendChild(row)
     })
 
-    if (cookies) {
-      const taskJSON = cookies.split("=")[1]
-
-      return JSON.parse(taskJSON)
-
-    }
-    return []
   }
 }
 
-function addToTable(task) {
-  const taskBody = document.querySelector("tbody")
-  const row = document.createElement("tr")
+const tareaManager = new TareaManager()
+tareaManager.readTasks()
 
-  row.innerHTML = `
-    <td>${task.id}</td>
-    <td>${task.description}</td>
-    <td class="d-grid gap-2">
-      <button class="btn btn-warning btn-sm" data-bs-toggle="modal"
-        data-bs-target="#addTaskModal">Modificar</button>
-      <button id="delTask" class="btn btn-danger btn-sm" data-bs-toggle="modal"
-        data-bs-target="#delTaskModal">Eliminar</button>
-    </td>
-  `
+let editingTaskId = null
+let taskToDelete = null
 
-  tBody.appendChild(row)
+function prepareAddTask() {
+  document.getElementById("modalForm").reset()
+  editingTaskId = null
+
 }
 
-function loadTask() {
-  const tasks = tareaManager.readTask()
-  tasks.forEach(function(task) {
-    addToTable(task)
+function prepareEditTask(id) {
+  const task = tareaManager.tasks.find(function(task) {
+    return task.id === id
   })
+
+  if(task) {
+    document.getElementById("description").value = task.getDescription()
+    editingTaskId = id
+
+  }
 }
 
 function addTask() {
   const description = document.getElementById("description").value
-  const newTask = tareaManager.addTask(description)
 
-  addToTable(newTask)
+  if(editingTaskId !== null) {
+    tareaManager.updateTask(editingTaskId, description)
+    editingTaskId = null
+  }else {
+    tareaManager.addTask(description)
+  }
+
   document.getElementById("description").value = ""
-
 }
 
-function updateTask() {
-  const task = tareaManager.readTasks().find(function(task) {
-    return task.id === id
-  })
-
-  document.getElementById("modalTitle").innerText = "Editar Tarea"
-  document.getElementById("doThingsBtn").innerText = "Actualizar Tarea"
-  document.getElementById("description").value = task.description
-  document.getElementById("doThingsBtn").onClick = function() {
-    tareaManager.updateTask(id, document.getElementById("description").value)
-    location.reload()
-
-  }
+function prepareDeleteTask(id) {
+  taskToDelete = id
 }
 
 function deleteTask(id) {
+  if(taskToDelete !== null) {
   tareaManager.deleteTask(id)
+  taskToDelete = null
   location.reload()
+  }
 }

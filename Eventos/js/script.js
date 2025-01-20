c7116604d83b0dd223c2ab49afe4bed2ab387b93
Francisco <paquitotejeroangel@gmail.com>
2025-01-20 10:10:24 +0100

    html done
    Reinterpretacion del login/register && adminPage terminada

--- Eventos/js/script.js
+++ Eventos/js/script.js
@@ -1,4 +1,17 @@
 
+$(document).ready(function(){
+  $("#showRegister").click(function(show){
+    show.preventDefault()
+    $("#loginForm").addClass("visually-hidden")
+    $("#registerForm").removeClass("visually-hidden")
+  })
+
+  $("#showLogin").click(function(show){
+    show.preventDefault()
+    $("#registerForm").addClass("visually-hidden")
+    $("#loginForm").removeClass("visually-hidden")
+  })
+})
 
 
 
